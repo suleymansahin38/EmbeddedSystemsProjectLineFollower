3ad713afe4863a2fd2b00c0b33bb692e5d5d1deb
suleymansahin38 <157317093+suleymansahin38@users.noreply.github.com>
2025-01-13 09:29:41 +0300

    Delete Project/ProjeSon directory

--- Project/ProjeSon/Contor.c
+++
@@ -1,4 +0,0 @@
-#include "Contor.h"
-
-Contor ContorInstance;       // Define the actual My_PID structure
-Contor *Controller = &ContorInstance; // Initialize the pointer to point to MyPIDInstance
--- Project/ProjeSon/Objects/projeson1.d
+++
@@ -1,6 +0,0 @@
-./objects/projeson1.o: Projeson1.c \
-  C:\Users\steam\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  RTE\_Target_1\RTE_Components.h \
-  C:\Users\steam\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\steam\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
-  Clockconfig.h delay.h Contor.h
